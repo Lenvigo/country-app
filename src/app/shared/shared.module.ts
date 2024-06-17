f999a6157f1151b4fb5836feece3b38f47be80e3
Elena Gonzalez <elena.gonzalezlago@plexus.es>
2024-06-17 18:00:38 +0200

    build: LazyLoad & SearchBox

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,6 +6,7 @@ import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { RouterModule } from '@angular/router';
 import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
 
 
 
@@ -15,6 +16,7 @@ import { ContactPageComponent } from './pages/contact-page/contact-page.componen
     HomePageComponent,
     SidebarComponent,
     ContactPageComponent,
+    SearchBoxComponent,
 
 
   ],
@@ -26,7 +28,8 @@ import { ContactPageComponent } from './pages/contact-page/contact-page.componen
     AboutPageComponent,
     HomePageComponent,
     SidebarComponent,
-    ContactPageComponent
+    ContactPageComponent,
+    SearchBoxComponent,
   ]
 })
 export class SharedModule { }
