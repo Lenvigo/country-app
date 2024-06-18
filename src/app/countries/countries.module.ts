33eac10cbe9674aad2cdc3241661b892458e998f
Elena Gonzalez <elena.gonzalezlago@plexus.es>
2024-06-18 10:23:32 +0200

    feat: mostrar por pantalla

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -7,6 +7,7 @@ import { CountryPageComponent } from './pages/country-page/country-page.componen
 import { CountriesRoutingModule } from './countries-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { SearchBoxComponent } from '../shared/components/search-box/search-box.component';
+import { CountryTableComponent } from './components/country-table/country-table.component';
 
 
 
@@ -15,7 +16,8 @@ import { SearchBoxComponent } from '../shared/components/search-box/search-box.c
     ByCapitalPageComponent,
     ByCountryPageComponent,
     ByRegionPageComponent,
-    CountryPageComponent
+    CountryPageComponent,
+    CountryTableComponent
   ],
   imports: [
     CommonModule,
