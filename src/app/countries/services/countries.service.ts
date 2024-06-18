7033e486d9e9c89bcbd4a271e575d9bc47c61e7f
Elena Gonzalez <elena.gonzalezlago@plexus.es>
2024-06-18 10:38:04 +0200

    manejo errores en observables

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,7 +1,7 @@
 
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 
 import { Country } from '../interfaces/country';
 
@@ -15,7 +15,10 @@ export class CountriesService {
 
   searchCapital(term: string): Observable<Country[]> {
     const url = `${this.apiUrl}/capital/${term}`;
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url)
+    .pipe(
+    catchError(error=>of([])) // catchError( () => of([]) )
+    );
   }
 
 }
