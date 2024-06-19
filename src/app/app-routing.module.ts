911c08392f038d45c9ef0dc86eadfbcd141a01ba
Elena Gonzalez <elena.gonzalezlago@plexus.es>
2024-06-19 17:08:58 +0200

    <Ngmodule><app-routing> providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy}]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './shared/pages/home-page/home-page.component';
 import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
 import { ContactPageComponent } from './shared/pages/contact-page/contact-page.component';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 const routes: Routes = [
   // {
@@ -11,19 +12,19 @@ const routes: Routes = [
   //   component: HomePageComponent
   // },
   {
-    path:'about',
+    path: 'about',
     component: AboutPageComponent
   },
   {
-    path:'contact',
+    path: 'contact',
     component: ContactPageComponent
   },
   {
-    path:'countries',
-    loadChildren: ()=>import('./countries/countries.module').then( m=> m.CountriesModule)
+    path: 'countries',
+    loadChildren: () => import('./countries/countries.module').then(m => m.CountriesModule)
   },
   {
-    path:'**',
+    path: '**',
     redirectTo: 'countries/by-capital'
   }
 ];
@@ -34,6 +35,12 @@ const routes: Routes = [
   ],
   exports: [
     RouterModule
+  ],
+  providers: [
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy
+    }
   ]
 })
 export class AppRoutingModule { }
